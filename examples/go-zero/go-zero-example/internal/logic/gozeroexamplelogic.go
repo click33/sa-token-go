08019e34633806323dc80603dced0274782aeb64
mervyn <starry99c@163.com>
2026-05-14 23:39:23 +0800

    chore: fix import order and update workspace sum

--- examples/go-zero/go-zero-example/internal/logic/gozeroexamplelogic.go
+++ examples/go-zero/go-zero-example/internal/logic/gozeroexamplelogic.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/zeromicro/go-zero/core/logx"
 
-	sazero "github.com/click33/sa-token-go/integrations/go-zero"
 	"github.com/click33/sa-token-go/examples/go-zero/go-zero-example/internal/svc"
 	"github.com/click33/sa-token-go/examples/go-zero/go-zero-example/internal/types"
+	sazero "github.com/click33/sa-token-go/integrations/go-zero"
 )
 
 type AuthLogic struct {
